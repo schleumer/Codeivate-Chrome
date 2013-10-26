6a2359e477352c2751696927554064e8d0cea4d0
BeryJu <jenslanghammer@gmail.com>
2013-10-26 11:20:00 +0200

    Fixed Popup DOM not updating

--- bin/options/options.js
+++ bin/options/options.js
@@ -2,11 +2,17 @@ document.getElementById("submit").onclick = function() {
 	var t = document.getElementById("username").value;
 	if (t !== "") {
 		localStorage['user'] = t;
-		var ex = new Codeivate.Extension(localStorage['user'], document);
-		ex.update();
-		ex.start();
 		document.getElementById("submit").disabled = true;
 		document.getElementById("username").disabled = true;
 		document.getElementById("submit").innerText = "Authenticated!";
 	}
 };
+
+window.onload = function() {
+	if (localStorage['user']) {
+		document.getElementById("submit").disabled = true;
+		document.getElementById("username").value = localStorage['user'];
+		document.getElementById("username").disabled = true;
+		document.getElementById("submit").innerText = "Authenticated!";
+	}
+}
