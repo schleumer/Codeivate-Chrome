f5c9f14336b1b77d2df1b08deda41f914bc9fe1b
Paul <p4ul.sincl4ir@gmail.com>
2013-10-29 21:01:55 +1300

    added better authentication

--- bin/options/options.js
+++ bin/options/options.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
 
 	$('#reset').click(function(){
+		console.log('resetting');
 		localStorage.clear();
+
+		$('#submit').prop('disabled',false).removeClass('disabled').button('reset');
+		$('#username').prop('disabled', false);
+		$('#submit').removeClass('btn-primary btn-success btn-warning');
+		 $('.form-group').removeClass('has-success');
+		$('#submit').addClass('btn-primary');
 	});
 
 	$('#submit').click(function(){
@@ -12,27 +19,30 @@ $(document).ready(function(){
 			var codeivate = new Codeivate.Extension(username, document);
 			codeivate.authenticate( function(response){
 
-				var data = JSON.parse(request.responseText);
+				var data = JSON.parse(response.responseText);
 
 				    if ( typeof data.error == "undefined") {
 						localStorage['user'] = username;
-						document.getElementById("submit").disabled = true;
-						document.getElementById("username").disabled = true;
+						$("submit").prop('disabled', true);
+						$("username").prop('disabled', true);
 						$('#submit').removeClass('btn-primary btn-success btn-warning');
 						$('#submit').addClass('btn-success');
 						
 						$('#submit').button('reset');
-	                           
+	                    $('.form-group').removeClass('has-error');
+	                    $('.form-group').addClass('has-success');
+	                    $('.username_error').empty();       
 	                } else {
 						$('#submit').removeClass('btn-primary btn-success btn-warning');
-						$('#submit').addClass('btn-warning');
-
+						$('#submit').addClass('btn-primary');
+						$('.form-group').addClass('has-error');
+						$('.username_error').html($('<strong>').html('enter a valid username'));
 						$('#submit').button('reset');
 	                    console.error("status code: " + response.status);
 	                }                
 	            });
 		} else {
-			alert('please enter username');
+			alert('please enter a username');
 			$('#submit').removeClass('btn-primary btn-success btn-warning');
 			$('#submit').addClass('btn-warning');
 			$('#submit').button('reset');
